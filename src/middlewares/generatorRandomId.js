4314df5aba88d7dd79e9790d7592b05cb2995c61
Roman <kaboom.ua@gmail.com>
2017-05-22 14:57:22 +0300

    Update generatorRandomId.js

--- src/middlewares/generatorRandomId.js
+++ src/middlewares/generatorRandomId.js
@@ -1,5 +1,6 @@
 import { ADD_COMMENT } from '../constants';
 
+    //через мидлвары будет проходить каждый экшин, они должны быть максимально общими, завязывать на конкретные экшины - не лучшее решение
 export default store => next => action => {
 
     if (action.type == ADD_COMMENT) {
